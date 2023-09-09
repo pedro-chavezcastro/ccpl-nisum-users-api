aaf9229b479e241dd0c859367d1b3e5245a83701
pedro-chavezcastro <pedro.chavezcastro@outlook.com>
2023-09-09 13:19:47 -0500

    first commit

---
+++ src/main/java/com/nisum/ccpl/ccplnisumusersapi/CcplNisumUsersApiApplication.java
@@ -0,0 +1,13 @@
+package com.nisum.ccpl.ccplnisumusersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcplNisumUsersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcplNisumUsersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nisum/ccpl/ccplnisumusersapi/CcplNisumUsersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.ccpl.ccplnisumusersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcplNisumUsersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
